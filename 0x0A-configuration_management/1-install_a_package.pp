6d26ada2785912d3b0e58b884e5c3516ecfe5d7d
patrice02 <patricedev02@gmail.com>
2023-09-22 23:39:37 +0000

    fixe error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,8 @@
 # install flask -v 2.1.0
 
-# Create a Puppet class for installing Flask
-class myapp::flask {
-  package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-  }
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
 
-# Include the class to install Flask
-include myapp::flask
 
