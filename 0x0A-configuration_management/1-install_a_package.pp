16f7d9eb2f458f053d77262bb234d6c3e1a51b9f
patrice02 <patricedev02@gmail.com>
2023-09-22 23:19:59 +0000

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# install puppet-lint -v 2.5.0
+# install flask -v 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Create a Puppet class for installing Flask
+class myapp::flask {
+  package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+  }
 }
+
+# Include the class to install Flask
+include myapp::flask
+
