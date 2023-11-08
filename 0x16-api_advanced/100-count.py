387275a313706138ff15ccbc11037cdd7d667456
patrice <patricedev02@gmail.com>
2023-11-08 00:32:16 +0000

    refactoring

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -16,7 +16,8 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {"after": after, "count": count, "limit": 100}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     try:
         results = response.json()
         if response.status_code == 404:
